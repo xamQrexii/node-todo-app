b26f8424f2970a3ab4bdd2861fcf00b611804922
xamqrexii <usama1630@gmail.com>
2019-10-14 15:33:18 +0500

    TodoSchema Created

---
+++ models/Todo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model(Todo, TodoSchema);
