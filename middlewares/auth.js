faf409516d7c89575b0c3dff07e8082955510986
xamqrexii <usama1630@gmail.com>
2019-10-14 23:41:46 +0500

    Auth middleware created with JWT verify and username verification

---
+++ middlewares/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../models/User');
+
+// environment variable setup
+const JWT_SECRET = process.env.JWT_SETRET || config.get('JWT_SECRET');
+
+module.exports = async (req, res, next) => {
+
+    // get token from header and check
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    try {
+        // verify token
+        const decoded = await jwt.verify(token, JWT_SECRET);
+
+        // check user in dataabase for security purpose
+        let existingUser = await User.findById(decoded.user.id);
+        if (!existingUser) {
+            return res.status(401).json({
+                success: false,
+                message: "Token is not valid"
+            });
+        }
+
+        // set user object in req
+        req.user = decoded.user;
+        next();
+
+    } catch (error) {
+
+        console.log('Error:', error.message);
+        res.status(401).json({ message: 'Token is not valid' })
+
+    }
+};
