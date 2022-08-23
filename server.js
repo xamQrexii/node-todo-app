c67974a9450ff6688b9106d8c6c9515b2abb76ed
xamqrexii <usama1630@gmail.com>
2022-08-23 09:49:12 +0500

    refactor: update port

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use((req, res) => res.status(404).send({ message: `API route not found`, rou
 
 
 // set port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
